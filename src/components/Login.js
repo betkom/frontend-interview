52b959b8404a0d13d45bb23eda98187c1af66879
betkom <betkomuniq@gmail.com>
2020-03-28 10:14:15 -0400

    allow user to login and add email validation

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,56 +2,61 @@ import * as React from "react";
 import { useSetUserContext } from "../contexts/user";
 
 const Login = () => {
-  const [user] = React.useState({
-    email: "",
-    password: ""
-  });
+  const [user, setUser] = React.useReducer(
+    (state, newState) => ({ ...state, ...newState }),
+    {email: "", password: ""}
+  );
   const [error, setError] = React.useState(null);
+
+  const isValidEmail = email => /\S+@\S+\.\S+/.test(email)
 
   React.useEffect(() => {
     setError(null);
   }, [user.email, user.password]);
 
   const setUserContext = useSetUserContext();
+
+  const onSubmit = (e) => {
+    setError(null);
+    e.preventDefault();
+
+    if (
+      user.email &&
+      isValidEmail(user.email) &&
+      user.password &&
+      user.password.trim() === "password"
+    ) {
+      setUserContext({
+        name: "Test User",
+        ...user
+      });
+    } else {
+      setError("invalid");
+    }
+  }
+
   return (
-    <>
+    <React.Fragment>
       <h1>Login</h1>
       {error && <p>Error: {error}</p>}
       <form
-        onSubmit={e => {
-          setError(null);
-          e.preventDefault();
-          if (
-            user.email &&
-            user.password &&
-            user.password.trim() === "password"
-          ) {
-            setUserContext({
-              name: "Test User",
-              ...user
-            });
-          } else {
-            setError("invalid");
-          }
-        }}
+        onSubmit={onSubmit}
       >
         <input
           name="email"
-          value={user.email}
           onChange={event => {
-            user.email = event.target.value;
+            setUser({email: event.target.value})
           }}
         />
         <input
           name="password"
-          value={user.password}
           onChange={event => {
-            user.password = event.target.value;
+            setUser({password: event.target.value})
           }}
         />
         <button type="submit">Login</button>
       </form>
-    </>
+    </React.Fragment>
   );
 };
 
